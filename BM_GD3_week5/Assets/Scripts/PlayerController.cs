f49b0d8d896a2397000debf58fd00dc88b9113a3
tinCrispy <basilmarples@outlook.com>
2024-08-28 13:27:22 +0100

    PowerUp
    get he power up and knock your opponent out of the ball park

--- BM_GD3_week5/Assets/Scripts/PlayerController.cs
+++ BM_GD3_week5/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,12 @@ public class PlayerController : MonoBehaviour
     public GameObject focalPoint;
     float lowerBoundY = -10;
     Vector3 startPos;
+
+    public bool hasPowerUp;
+    public int repelForce;
+
+    public GameObject powerUpIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,10 +34,49 @@ public class PlayerController : MonoBehaviour
             transform.position = startPos;
             rb.angularVelocity = Vector3.zero;
             rb.velocity = Vector3.zero;
+            hasPowerUp = false;
+            powerUpIndicator.SetActive(false);
+        }
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.4f, 0);
+        powerUpIndicator.transform.Rotate(0, 45 * Time.deltaTime, 0);
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+            {
+            Destroy(other.gameObject);
+            hasPowerUp = true;
+            powerUpIndicator.SetActive(true);
+            StartCoroutine(PowerUpCountDownRoutine());
         }
 
     }
-   
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+
+            Vector3 repel = (collision.gameObject.transform.position - transform.position).normalized;
+
+            enemyRb.AddForce(repel * repelForce, ForceMode.Impulse);
+        }
+
+    }
+
+    IEnumerator PowerUpCountDownRoutine()
+    {
+        yield return new WaitForSeconds(10);
+        hasPowerUp = false;
+        powerUpIndicator.SetActive(false);
+    }
+
 
 
 
